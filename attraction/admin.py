641f32619dcd8fc872c4a9e9fc1ee5ddd534ed76
Joutce <ojoutace@gmail.com>
2023-08-22 15:35:41 -0300

    Add new structure files and models

---
+++ attraction/admin.py
@@ -0,0 +1,4 @@
+from .models import IAttraction
+from django.contrib import admin
+
+admin.site.register(IAttraction)
---
+++ attraction/models.py
@@ -0,0 +1,13 @@
+"Native models lib import"
+from django.db import models
+
+
+class IAttraction(models.Model):
+    "Class representing a tourist point"
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    working_period = models.TextField()
+    min_age = models.IntegerField()
+
+    def _str_(self):
+        return self.name
